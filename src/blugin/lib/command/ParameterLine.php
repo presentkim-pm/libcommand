69dc51051fbdebde20d2bee9263f9c32153e0fa9
PresentKim <debe3721@gmail.com>
2020-08-23 19:50:46 +0900

    Init ParameterLine

---
+++ src/blugin/lib/command/ParameterLine.php
@@ -0,0 +1,76 @@
+<?php
+
+/*
+ *
+ *  ____  _             _         _____
+ * | __ )| |_   _  __ _(_)_ __   |_   _|__  __ _ _ __ ___
+ * |  _ \| | | | |/ _` | | '_ \    | |/ _ \/ _` | '_ ` _ \
+ * | |_) | | |_| | (_| | | | | |   | |  __/ (_| | | | | | |
+ * |____/|_|\__,_|\__, |_|_| |_|   |_|\___|\__,_|_| |_| |_|
+ *                |___/
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the MIT License.
+ *
+ * @author  Blugin team
+ * @link    https://github.com/Blugin
+ * @license https://www.gnu.org/licenses/mit MIT License
+ *
+ *   (\ /)
+ *  ( . .) ♥
+ *  c(")(")
+ */
+
+declare(strict_types=1);
+
+namespace blugin\lib\command;
+
+use blugin\lib\command\parameter\Parameter;
+
+class ParameterLine{
+    /** @var BaseCommand */
+    protected $baseCommand;
+
+    /** @var Parameter[] */
+    protected $parameters = [];
+
+    /** @var int */
+    protected $requireLength = 0;
+
+    public function __construct(BaseCommand $baseCommand){
+        $this->baseCommand = $baseCommand;
+    }
+
+    public function getBaseCommand() : BaseCommand{
+        return $this->baseCommand;
+    }
+
+    /**
+     * @return Parameter[]
+     */
+    public function getParameters() : array{
+        return $this->parameters;
+    }
+
+    public function addParamater(Parameter $parameter) : ParameterLine{
+        $index = count($this->parameters);
+        $before = $this->parameters[$index - 1] ?? null;
+        if($before instanceof Parameter){
+            if($before->getLength() === PHP_INT_MAX)
+                throw new \RuntimeException("You can't register parameter after infinite-length parameter");
+
+            if($before->isOptional() && !$parameter->isOptional())
+                throw new \RuntimeException("You can't register not-optional parameter after optional parameter");
+        }
+
+        $this->parameters[] = $parameter;
+        if(!$parameter->isOptional()){
+            $this->requireLength = count($this->parameters);
+        }
+        return $this;
+    }
+
+    public function getRequireLength() : int{
+        return $this->requireLength;
+    }
+}
