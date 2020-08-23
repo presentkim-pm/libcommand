3847eb30abec32486633a5c31840a9b0b1e3ad57
PresentKim <debe3721@gmail.com>
2020-08-23 16:27:26 +0900

    Add default parameters

---
+++ src/blugin/lib/command/parameter/Parameter.php
@@ -0,0 +1,147 @@
+<?php
+
+/*
+ *
+ *  ____  _             _         _____
+ * | __ )| |_   _  __ _(_)_ __   |_   _|__  __ _ _ __ ___
+ * |  _ \| | | | |/ _` | | '_ \    | |/ _ \/ _` | '_ ` _ \
+ * | |_) | | |_| | (_| | | | | |   | |  __/ (_| | | | | | |
+ * |____/|_|\__,_|\__, |_|_| |_|   |_|\___|\__,_|_| |_| |_|
+ *                |___/
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the MIT License.
+ *
+ * @author  Blugin team
+ * @link    https://github.com/Blugin
+ * @license https://www.gnu.org/licenses/mit MIT License
+ *
+ *   (\ /)
+ *  ( . .) ♥
+ *  c(")(")
+ */
+
+declare(strict_types=1);
+
+namespace blugin\lib\command\parameter;
+
+use blugin\lib\command\BaseCommand;
+use blugin\lib\command\ParameterLine;
+use pocketmine\command\CommandSender;
+use pocketmine\network\mcpe\protocol\AvailableCommandsPacket;
+use pocketmine\network\mcpe\protocol\types\command\CommandEnum;
+use pocketmine\network\mcpe\protocol\types\command\CommandParameter;
+
+abstract class Parameter extends CommandParameter{
+    /** @var ParameterLine|null */
+    protected $owningLine = null;
+
+    /** @var int length of parameter */
+    protected $length = 1;
+
+    public function __construct(?ParameterLine $owningLine, string $name = null, bool $optional = false, ?CommandEnum $enum = null){
+        $this->owningLine = $owningLine;
+
+        $this->paramName = $name;
+        $this->isOptional = $optional;
+        $this->enum = $enum;
+        $this->paramType = $this->getParamType();
+    }
+
+    public function getOwningLine() : ?ParameterLine{
+        return $this->owningLine;
+    }
+
+    public function setOwningLine(ParameterLine $owningLine) : Parameter{
+        $this->owningLine = $owningLine;
+        return $this;
+    }
+
+    public function getBaseCommand() : ?BaseCommand{
+        return $this->owningLine !== null ? $this->owningLine->getBaseCommand() : null;
+    }
+
+    /**
+     * @param string[] $params
+     */
+    public function sendMessage(CommandSender $sender, string $str, array $params = []) : void{
+        $this->getBaseCommand()->sendMessage($sender, $str, $params);
+    }
+
+    public function getName() : ?string{
+        return $this->paramName;
+    }
+
+    public function setName(?string $name) : Parameter{
+        $this->paramName = $name;
+        return $this;
+    }
+
+    public function getParamType() : int{
+        return $this->getType() | AvailableCommandsPacket::ARG_FLAG_VALID;
+    }
+
+    public function isOptional() : bool{
+        return $this->isOptional;
+    }
+
+    public function setOptional(bool $isOptional) : Parameter{
+        $this->isOptional = $isOptional;
+        return $this;
+    }
+
+    public function getFlags() : int{
+        return $this->flags;
+    }
+
+    public function getEnum() : ?CommandEnum{
+        return $this->enum;
+    }
+
+    public function setEnum(?CommandEnum $enum) : Parameter{
+        $this->enum = $enum;
+        return $this;
+    }
+
+    public function getPostfix() : ?string{
+        return $this->postfix;
+    }
+
+    public function setPostfix(?string $postfix) : Parameter{
+        $this->postfix = $postfix;
+        return $this;
+    }
+
+    public function getLength() : int{
+        return $this->length;
+    }
+
+    public function setLength(int $length) : Parameter{
+        $this->length = $length;
+        return $this;
+    }
+
+    public function prepare() : Parameter{
+        return $this;
+    }
+
+    /**
+     * @param string[] $args the remained parameters
+     */
+    public function valid(CommandSender $sender, string $argument) : bool{
+        return $this->parse($sender, $argument) !== null;
+    }
+
+    /**
+     * @param string[] $args the remained parameters
+     *
+     * @return string
+     */
+    public function parse(CommandSender $sender, string $argument){
+        return $argument;
+    }
+
+    abstract public function getType() : int;
+
+    abstract public function getTypeName() : string;
+}
---
+++ src/blugin/lib/command/parameter/Vector3Parameter.php
@@ -0,0 +1,95 @@
+<?php
+
+/*
+ *
+ *  ____  _             _         _____
+ * | __ )| |_   _  __ _(_)_ __   |_   _|__  __ _ _ __ ___
+ * |  _ \| | | | |/ _` | | '_ \    | |/ _ \/ _` | '_ ` _ \
+ * | |_) | | |_| | (_| | | | | |   | |  __/ (_| | | | | | |
+ * |____/|_|\__,_|\__, |_|_| |_|   |_|\___|\__,_|_| |_| |_|
+ *                |___/
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the MIT License.
+ *
+ * @author  Blugin team
+ * @link    https://github.com/Blugin
+ * @license https://www.gnu.org/licenses/mit MIT License
+ *
+ *   (\ /)
+ *  ( . .) ♥
+ *  c(")(")
+ */
+
+declare(strict_types=1);
+
+namespace blugin\lib\command\parameter;
+
+use pocketmine\command\CommandSender;
+use pocketmine\math\Vector3;
+use pocketmine\network\mcpe\protocol\AvailableCommandsPacket;
+
+class Vector3Parameter extends Parameter{
+    /** @var bool Whether to rounding down the coordinates */
+    protected $isFloor = false;
+
+    public function getType() : int{
+        return AvailableCommandsPacket::ARG_TYPE_POSITION;
+    }
+
+    public function getTypeName() : string{
+        return "x y z";
+    }
+
+    public function prepare() : Parameter{
+        $this->setLength(3);
+        return $this;
+    }
+
+    /**
+     * @param string[] $args the remained parameters
+     */
+    public function valid(CommandSender $sender, string $argument) : bool{
+        $pattern = "^" . ($sender instanceof Vector3 ? "(~|~\+)?" : "") . "-?(\d+|\d*\.\d+)$";
+        foreach(explode(" ", $argument) as $coord){
+            if(!preg_match($pattern, $coord))
+                return false;
+        }
+        return true;
+    }
+
+    /**
+     * @return Vector3|null
+     */
+    public function parse(CommandSender $sender, string $argument){
+        if(!$this->valid($sender, $argument))
+            return null;
+
+        $argument = explode(" ", $argument);
+        $coords = [
+            "x" => $argument[0],
+            "y" => $argument[1],
+            "z" => $argument[2]
+        ];
+        foreach($coords as $coordName => &$coord){
+            if($sender instanceof Vector3 && strpos($coord, "~") === 0){
+                $coord = $sender->{$coordName} + (float) substr($coord, 1);
+            }else{
+                $coord = (float) $coord;
+            }
+
+            if($this->isFloor()){
+                $coord = (int) $coord;
+            }
+        }
+        return new Vector3(...$coords);
+    }
+
+    public function isFloor() : bool{
+        return $this->isFloor;
+    }
+
+    public function setFloor(bool $isFloor) : void{
+        $this->isFloor = $isFloor;
+    }
+}
