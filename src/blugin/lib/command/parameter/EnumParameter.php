d9b016687bf4fb970ab8a80bb4923b334f05a985
PresentKim <debe3721@gmail.com>
2020-08-23 16:27:26 +0900

    Add default parameters

---
+++ src/blugin/lib/command/parameter/EnumParameter.php
@@ -0,0 +1,88 @@
+<?php
+
+/*
+ *
+ *  ____  _             _         _____
+ * | __ )| |_   _  __ _(_)_ __   |_   _|__  __ _ _ __ ___
+ * |  _ \| | | | |/ _` | | '_ \    | |/ _ \/ _` | '_ ` _ \
+ * | |_) | | |_| | (_| | | | | |   | |  __/ (_| | | | | | |
+ * |____/|_|\__,_|\__, |_|_| |_|   |_|\___|\__,_|_| |_| |_|
+ *                |___/
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the MIT License.
+ *
+ * @author  Blugin team
+ * @link    https://github.com/Blugin
+ * @license https://www.gnu.org/licenses/mit MIT License
+ *
+ *   (\ /)
+ *  ( . .) ♥
+ *  c(")(")
+ */
+
+declare(strict_types=1);
+
+namespace blugin\lib\command\parameter;
+
+use pocketmine\command\CommandSender;
+
+abstract class EnumParameter extends Parameter{
+    /** @var bool Whether it should be written in exactly full name */
+    protected $exact = true;
+
+    /** @var bool Whether to check case */
+    protected $caseSensitive = false;
+
+    /**
+     * @return string|null
+     */
+    public function parse(CommandSender $sender, string $argument){
+        if($this->enum !== null){
+            if($this->isExact()){
+                foreach($this->enum->getValues() as $value){
+                    if(($this->isCaseSensitive() ? strcmp($argument, $value) : strcasecmp($argument, $value)) === 0){
+                        return $value;
+                    }
+                }
+                return null;
+            }
+
+            $found = null;
+            $length = strlen($argument);
+            $minDiff = PHP_INT_MAX;
+            foreach($this->enum->getValues() as $value){
+                if(($this->isCaseSensitive() ? strpos($argument, $value) : stripos($argument, $value)) === 0){
+                    $diff = strlen($value) - $length;
+                    if($diff < $minDiff){
+                        $found = $value;
+                        if($diff === 0)
+                            break;
+
+                        $minDiff = $diff;
+                    }
+                }
+            }
+            return $found;
+        }
+        return null;
+    }
+
+    public function isExact() : bool{
+        return $this->exact;
+    }
+
+    public function setExact(bool $exact) : EnumParameter{
+        $this->exact = $exact;
+        return $this;
+    }
+
+    public function isCaseSensitive() : bool{
+        return $this->caseSensitive;
+    }
+
+    public function setCaseSensitive(bool $caseSensitive) : EnumParameter{
+        $this->caseSensitive = $caseSensitive;
+        return $this;
+    }
+}
---
+++ src/blugin/lib/command/parameter/PlayerParameter.php
@@ -0,0 +1,76 @@
+<?php
+
+/*
+ *
+ *  ____  _             _         _____
+ * | __ )| |_   _  __ _(_)_ __   |_   _|__  __ _ _ __ ___
+ * |  _ \| | | | |/ _` | | '_ \    | |/ _ \/ _` | '_ ` _ \
+ * | |_) | | |_| | (_| | | | | |   | |  __/ (_| | | | | | |
+ * |____/|_|\__,_|\__, |_|_| |_|   |_|\___|\__,_|_| |_| |_|
+ *                |___/
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the MIT License.
+ *
+ * @author  Blugin team
+ * @link    https://github.com/Blugin
+ * @license https://www.gnu.org/licenses/mit MIT License
+ *
+ *   (\ /)
+ *  ( . .) ♥
+ *  c(")(")
+ */
+
+declare(strict_types=1);
+
+namespace blugin\lib\command\parameter;
+
+use pocketmine\network\mcpe\protocol\AvailableCommandsPacket;
+use pocketmine\network\mcpe\protocol\types\command\CommandEnum;
+use pocketmine\Server;
+
+class PlayerParameter extends EnumParameter{
+    /** @var bool Whether to include offline players */
+    protected $includeOffline = false;
+
+    public function getType() : int{
+        return AvailableCommandsPacket::ARG_TYPE_TARGET;
+    }
+
+    public function getTypeName() : string{
+        return "target";
+    }
+
+    public function prepare() : Parameter{
+        $playerNames = [];
+        foreach(Server::getInstance()->getOnlinePlayers() as $key => $player){
+            $playerName = $player->getName();
+            $playerNames[strtolower($playerName)] = $playerName;
+        }
+        if($this->isIncludeOffline()){
+            foreach(scandir(Server::getInstance()->getDataPath() . "players/") as $key => $fileName){
+                if(substr($fileName, -4) === ".dat"){
+                    $playerName = substr($fileName, 0, -4);
+                    if(!isset($playerNames[strtolower($playerName)])){
+                        $playerNames[strtolower($playerName)] = $playerName;
+                    }
+                }
+            }
+        }
+        $this->enum = new CommandEnum("target", array_map(function(string $playerName) : string{
+            if(strpos($playerName, " ") !== false)
+                return "\"$playerName\"";
+
+            return $playerName;
+        }, array_values($playerNames)));
+        return $this;
+    }
+
+    public function isIncludeOffline() : bool{
+        return $this->includeOffline;
+    }
+
+    public function setIncludeOffline(bool $includeOffline) : void{
+        $this->includeOffline = $includeOffline;
+    }
+}
