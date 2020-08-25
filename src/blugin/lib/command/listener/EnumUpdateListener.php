94281856c0970ae9ca01918315b955b34862f5b3
PresentKim <debe3721@gmail.com>
2020-08-25 11:11:04 +0900

    Move event listeners to listener/

--- src/blugin/lib/command/listener/EnumUpdateListener.php
+++ src/blugin/lib/command/listener/EnumUpdateListener.php
@@ -25,7 +25,7 @@
 
 declare(strict_types=1);
 
-namespace blugin\lib\command\traits;
+namespace blugin\lib\command\listener;
 
 use blugin\lib\command\enum\Enum;
 use blugin\lib\command\enum\EnumFactory;
